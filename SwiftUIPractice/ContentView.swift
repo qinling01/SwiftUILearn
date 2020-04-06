9c5a950250bbf639a9351fe65248e70069724c2d
QLing <qinlingdj@icloud.com>
2020-04-06 12:35:32 +0800

    Initial Commit

---
+++ SwiftUIPractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIPractice
+//
+//  Created by 灵 Q on 2020/4/6.
+//  Copyright © 2020 灵 Q. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
